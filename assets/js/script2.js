5c91a98a1893f04d530e217780932fb83867d263
Nuwan Rathnayake <x@nuwanr.com>
2023-05-13 21:37:34 +0530

    animations

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,18 +1,8 @@
 $(document).ready(() => {
     $('#btn1').click(() => {
-        
-        $('.box1').slideUp(1000);
-        $('.box2').slideDown(1000);
-
-    });
-    $('#btn2').click(() => {
-        
-        $('.box2').slideUp(1000);
-        $('.box1').slideDown(1000);
-    });
-    $('#btn3').click(() => {
-        
-        $('.box2').slideToggle(1000);
-        $('.box1').slideToggle(1000);
+       $('.box1').animate({
+        height: '500px',
+        width: '500px'
+       }, 3000)
     });
 });
